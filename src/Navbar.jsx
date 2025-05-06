9953420a53815ae3b26a1602a63d1c4316d6edda
oweeeenie <owensullivan419@yahoo.com>
2025-05-05 22:43:38 -0400

    started navbar, added a few images & finished up the slim black header

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,11 +5,45 @@ export default function Navbar() {
     <>
       <div className='shadow-lg'>
         {/* NAVBAR INNER CONTAINER */}
-        <div className='py-2 px-4 flex items-center text-lg font-semibold gap-4'>
+        <div className='py-2 px-4 flex items-center text-lg font-semibold'>
           <img src={psLogo} alt='Blue PS Logo' className='h-13' />
           {/* NAVBAR DROPDOWNS */}
-          <button className='flex items-center'>
+          <button className='flex items-center px-5'>
             Games
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              className='h-4 w-4 pl-0.5'
+              fill='none'
+              viewBox='0 0 24 12'
+              stroke='gray'
+              strokeWidth={4}
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M19 9l-7 7-7-7'
+              />
+            </svg>
+          </button>
+          <button className='flex items-center px-5'>
+            PS5
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              className='h-4 w-4 pl-0.5'
+              fill='none'
+              viewBox='0 0 24 12'
+              stroke='gray'
+              strokeWidth={4}
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M19 9l-7 7-7-7'
+              />
+            </svg>
+          </button>
+          <button className='flex items-center px-5'>
+            PS4
             <svg
               xmlns='http://www.w3.org/2000/svg'
               className='h-4 w-4 pl-0.5'
